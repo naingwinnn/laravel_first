4637725d430074e49ad673980f302d3aac514da6
Naing Win <uunaing111@gmail.com>
2025-12-15 18:27:14 +0630

    Add ArticleController and article views

---
+++ database/factories/EnrollmentFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Enrollment>
+ */
+class EnrollmentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'student_id' => rand(1, 10),
+            'course_id' => rand(1, 5),
+            'enrolled_at' => fake()->date(),
+        ];
+    }
+
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,10 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Student;
+use App\Models\Course;
+use App\Models\Enrollment;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,13 +17,11 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
+    
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-    }
+    Student::factory(10)->create();
+    Course::factory(10)->create();
+    Enrollment::factory(10)->create();
+}
 }
