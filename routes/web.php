f89ba357a381bcdd4969d9370fcd7aa4f5963177
Naing Win <uunaing111@gmail.com>
2026-01-09 08:58:32 +0630

    added laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,54 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ArticleController;
-use App\Http\Controllers\StudentController;
-use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//static route
-Route::get('/hello', function () {
-    return 'Hello World';
-});
-Route::get('/articles/detail', function () {
-    return 'Article Detail';
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-//dynamic route
-Route::get('/articles/detail/{id}', function ($id) {
- return "Article Detail - $id";
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-//route name
-Route::get('/articles/detail', function () {
- return 'Article Detail';
-})->name('article.detail');
-
-//redirect function
-Route::get('/articles/more', function() {
- return redirect()->route('article.detail');
-});
-
-Route::get('/article', [ArticleController::class, 'showList'])->name('article.list');
-Route::get('/read/{articleId}', [ArticleController::class, 'read'])->name('article.read');
-
-Route::get('/students', [StudentController::class, 'allStudents']);
-Route::get('/students/find/{id}', [StudentController::class, 'findStudent']);
-Route::get('/students/filter', [StudentController::class, 'filterStudents']);
-Route::get('/students/first', [StudentController::class, 'firstStudent']);
-Route::get('/students/order', [StudentController::class, 'orderStudents']);
-Route::get('/students/pluck', [StudentController::class, 'pluckStudents']);
-Route::get('/students/create', [StudentController::class, 'createStudent']);
-Route::get('/students/update/{id}', [StudentController::class, 'updateStudent']);
-Route::get('/students/delete/{id}', [StudentController::class, 'deleteStudent']);
-
-Route::get('/test/one-to-one', [UserController::class, 'oneToOne']);
-Route::get('/test/one-to-many', [UserController::class, 'oneToMany']);
-Route::get('/test/many-to-many', [UserController::class, 'manyToMany']);
-Route::get('/test/has-one-through', [UserController::class, 'hasOneThrough']);
-Route::get('/test/has-many-through', [UserController::class, 'hasManyThrough']);
-
-?>
+require __DIR__.'/auth.php';
